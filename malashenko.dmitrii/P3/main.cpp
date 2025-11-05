a5e8f6d2eeae5f44806253b815fd555062b239c0
yahada <niadzy14@gmail.com>
2025-11-05 18:37:17 +0300

    task3 fixed & task4 made

--- malashenko.dmitrii/P3/main.cpp
+++ malashenko.dmitrii/P3/main.cpp
@@ -23,6 +23,8 @@ namespace malasenko {
     int * nums;
   };
   
+  
+
 
   matrix readMtx(std::istream & in) {
     matrix mtx;
@@ -44,21 +46,22 @@ namespace malasenko {
     return mtx;
   }
 
+
   int cntLocMax(int * mtx, size_t rows, size_t cols) {
     int res = 0;
 
     for (size_t i = 0; i < rows; ++i) {
       for (size_t j = 0; j < cols; ++j) {
-        if ((i != 0 || i != rows) && (j != 0 || j != cols)) {
-          int num = mtx[i*rows + j];
-          bool isLocMax = (num > mtx[(i+1)*rows + j]);
-          isLocMax = isLocMax || (num > mtx[(i-1)*rows + j]);
-          isLocMax = isLocMax || (num > mtx[(i)*rows + (j-1)]);
-          isLocMax = isLocMax || (num > mtx[(i)*rows + (j+1)]);
-          isLocMax = isLocMax || (num > mtx[(i+1)*rows + (j+1)]);
-          isLocMax = isLocMax || (num > mtx[(i+1)*rows + (j-1)]);
-          isLocMax = isLocMax || (num > mtx[(i-1)*rows + (j+1)]);
-          isLocMax = isLocMax || (num > mtx[(i-1)*rows + (j-1)]);
+        if ((i != 0 && i != (rows-1)) && (j != 0 && j != (cols-1))) {
+          int num = mtx[i * cols + j];
+          bool isLocMax = 0;
+          for (int i_ind = -1; i_ind <= 1; ++i_ind) {
+            for (int j_ind = -1; j_ind <= 1; ++j_ind) {
+              if (i_ind != 0 && j_ind != 0) {
+                isLocMax = isLocMax || (num > mtx[(i + i_ind) * cols + (j + j_ind)]);
+              }
+            }
+          }
           res+=isLocMax;
         }
       }
@@ -68,10 +71,52 @@ namespace malasenko {
   }
 
   int lftBotClk(int ** mtx, size_t rows, size_t cols) {
-    
-  }
+    if (cols == 0 || rows == 0) return;
 
+    size_t total = rows * cols;
+    int i = rows - 1, j = 0;
+    int step = 0, cnt = 0;
+
+    int top = 0,  botom = rows - 1, left = 0, right = cols - 1;
+
+      
+    while (cnt != total) {
+      while (i > top && cnt < total) {
+        step++;
+        mtx[i * cols + j] -= step;
+        cnt++;
+        i--; 
+      }
+      left++;
+        
+      while (j < right && cnt < total) {
+        step++;
+        mtx[i * cols + j] -= step;
+        cnt++;
+        j++; 
+      }
+      top++;
+
+      while (i < botom && cnt < total) {
+        step++;
+        mtx[i * cols + j] -= step;
+        cnt++;
+        i++; 
+      }
+      right--;
+      
+      while (j > left && cnt < total) {
+        step++;
+        mtx[i * cols + j] -= step;
+        cnt++;
+        j--; 
+      }
+      botom--;
+    }
+  }
+  
 }
+
 
 int main() {
 
