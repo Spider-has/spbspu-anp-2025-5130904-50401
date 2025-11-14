a4af36b16521728d36338f8dea48c45e7c5a1ca3
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2025-11-14 19:24:15 +0300

    error checking

--- madieva.tatyana/P3/main.cpp
+++ madieva.tatyana/P3/main.cpp
@@ -1,3 +1,26 @@
-int main()
+#include <iostream>
+#include <fstream>
+namespace madieva
 {
+
+}
+
+int main(int argc, char ** argv)
+{
+  if (argc < 4) {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  }
+  if (argc > 4) {
+    std::cerr << "Too many arguments\n";
+    return 1;
+  }
+  if (argv[1] > 2 || argv[1] == 0) {
+    std::cerr << "First pharameter is out of range\n";
+    return 1;
+  }
+  if (argv[1] != 1 || argv != 2) {
+    std::cerr << "First parameter us not a number\n";
+  }
+  
 }
