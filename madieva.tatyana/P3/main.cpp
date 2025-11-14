0db7dcdf14556f720f655b814f4179fd4eef313c
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2025-11-14 22:12:47 +0300

    create and delete

--- madieva.tatyana/P3/main.cpp
+++ madieva.tatyana/P3/main.cpp
@@ -2,7 +2,24 @@
 #include <fstream>
 namespace madieva
 {
-
+  const size_t max_static_size = 10000;
+  int * create_array(char number, size_t rows, size_t cols) {
+    if (number == '1') {
+      if (rows * cols > max_static_size) {
+        throw std::bad_alloc();
+      }
+      static int array[max_static_size] = {};
+      return array;
+    }
+    else {
+      return new int[rows * cols];
+    }
+  }
+  void free_array(int * array, char number) {
+    if (number ==  '2') {
+      delete []array;
+    }
+  }  
 }
 
 int main(int argc, char ** argv)
@@ -15,12 +32,48 @@ int main(int argc, char ** argv)
     std::cerr << "Too many arguments\n";
     return 1;
   }
-  if (argv[1] > 2 || argv[1] == 0) {
+  if (argv[1][0] < '0' || argv[1][0] > '9') {
+    std::cerr << "First parameter us not a number\n";
+    return 1;
+  }
+  if (argv[1][0] != '1' && argv[1][0] != '2') {
     std::cerr << "First pharameter is out of range\n";
     return 1;
   }
-  if (argv[1] != 1 || argv != 2) {
-    std::cerr << "First parameter us not a number\n";
+  const char* arg = argv[1];
+  const char* inputFile = argv[2];
+  const char* outputFile = argv[3];
+
+
+  std::ifstream input(inputFile);
+  if (!input.is_open()) {
+    std::cerr << "Error when opening input file\n";
+    return 2;
   }
-  
+  size_t rows = 0, cols = 0;
+  input >> rows >> cols;
+  if (input.fail()) {
+    std::cerr << "Irregular matrix size\n";
+    return 2;
+  }
+  if ((rows == 0 && cols != 0) || (rows != 0 && cols == 0)) {
+    std::cerr << "Irregular matrix size\n";
+    return 2;
+  }
+  if (rows == 0 && cols == 0) {
+    std::ofstream output(outputFile);
+    output << 0 << 0 << "\n"; 
+    return 0;
 }
+  int* matrix = nullptr;
+  try {
+    matrix = madieva::create_array(arg[0], rows, cols);
+  } catch (const std::bad_alloc& e) {
+    std::cerr << "Out of memory\n";
+    return 2;
+  }
+  input.close();
+  madieva::free_array(matrix, arg[0]);
+  return 0;
+}
+
