c2eff9a706ef6290d0697ca4b3fe130608f900b6
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-11-16 16:36:18 +0300

    complete task with array of strings

--- zharov.danil/P4/main.cpp
+++ zharov.danil/P4/main.cpp
@@ -4,24 +4,30 @@
 
 namespace zharov
 {
-  char ** getArrStr(std::istream & in, size_t size_arr, size_t step_arr, size_t & len_arr, const char end);
+  char ** getArrStr(std::istream & in, 
+  size_t size_arr, size_t step_arr, size_t & len_arr, const char end_arr,
+  size_t size, size_t step, const char end);
   char * getLine(std::istream & in, size_t size, size_t step, size_t & len, const char end);
   void extend(char ** str, size_t old_size, size_t new_size);
   char * LatRmv(const char * str, char * res);
   char * ShtSym(const char * str, char * res);
+  void destroyArr(char ** arr, size_t len);
 }
 
 int main()
 {
+  size_t size_arr = 10;
+  size_t step_arr = 5;
+  size_t len_arr = 0;
+  const char end_arr = '\n';
   size_t size = 10;
   size_t step = 5;
-  size_t len = 0;
-  const char end = '\n';
-  char * str = nullptr;
+  const char end = ' ';
+  char ** arr_str = nullptr;
   try {
-    str = zharov::getLine(std::cin, size, step, len, end);
+    arr_str = zharov::getArrStr(std::cin, size_arr, step_arr, len_arr, end_arr, size, step, end);
     if (!std::cin) {
-      delete[] str;
+      zharov::destroyArr(arr_str, len_arr);
       std::cerr << "Bad enter\n";
       return 1;
     }
@@ -29,22 +35,50 @@ int main()
     std::cerr << "Bad alloc\n";
     return 1;
   }
-  char * res_1 = new char[len + 1];
-  char * res_2 = new char[27];
-  try {
-    res_1 = zharov::LatRmv(str, res_1);
-    res_2 = zharov::ShtSym(str, res_2);
-  } catch(const std::bad_alloc &) {
-    delete[] str;
+
+  for (size_t i = 0; i < len_arr; ++i) {
+    char * str = arr_str[i];
+    size_t c = 0;
+    while (str[c] != '\0') {
+      ++c;
+    }
+    char * res_1 = new char[c + 1];
+    try {
+      res_1 = zharov::LatRmv(str, res_1);
+    } catch(const std::bad_alloc &) {
+      delete[] str;
+      delete[] res_1;
+      std::cerr << "Bad alloc\n";
+      return 1;
+    }
+    std::cout << "LAT-RMV №" << i + 1 << ": " << res_1 << "\n";
     delete[] res_1;
-    std::cerr << "Bad alloc\n";
-    return 1;
   }
-  std::cout << "LAT-RMV: " << res_1 << "\n";
-  std::cout << "SHT_SYM: " << res_2 << "\n";
-  delete[] res_1;
-  delete[] res_2;
-  delete[] str;
+  std::cout << '\n';
+  for (size_t i = 0; i < len_arr; ++i) {
+    char * str = arr_str[i];
+    char * res_2 = new char[27];
+    try {
+      res_2 = zharov::ShtSym(str, res_2);
+    } catch(const std::bad_alloc &) {
+      delete[] str;
+      delete[] res_2;
+      std::cerr << "Bad alloc\n";
+      return 1;
+    }
+    std::cout << "SHT_SYM №" << i + 1 << ": " << res_2 << "\n";
+    delete[] res_2;
+  }
+  
+  zharov::destroyArr(arr_str, len_arr);
+}
+
+void zharov::destroyArr(char ** arr, size_t len)
+{
+  for (size_t i = 0; i < len; ++i) {
+    delete[] arr[i];
+  }
+  delete[] arr;
 }
 
 void zharov::extend(char ** str, size_t old_size, size_t new_size)
@@ -57,8 +91,9 @@ void zharov::extend(char ** str, size_t old_size, size_t new_size)
   * str = new_str;
 }
 
-char ** getArrStr(std::istream & in, size_t size_arr, size_t step_arr,
-  size_t & len_arr, const char end)
+char ** zharov::getArrStr(std::istream & in, 
+  size_t size_arr, size_t step_arr, size_t & len_arr, const char end_arr,
+  size_t size, size_t step, const char end)
 {
   char ** arr_str = new char * [size_arr];
   bool is_skipws = in.flags() & std::ios_base::skipws;
@@ -67,28 +102,26 @@ char ** getArrStr(std::istream & in, size_t size_arr, size_t step_arr,
   }
 
   char sym = ' ';
-  while (in >> sym && sym != end) {
+  while (in >> sym && sym != end_arr) {
     in.putback(sym);
     size_t str_len = 0;
     char * str = nullptr;
     try {
-      str = zharov::getLine(in, 10, 5, str_len, ' ');
+      str = zharov::getLine(in, size, step, str_len, end);
     } catch (const std::bad_alloc &) {
-      for (size_t i = 0; i < len_arr; ++i) {
-        delete[] arr_str[i];
-      }
-      delete[] arr_str;
+      zharov::destroyArr(arr_str, len_arr);
       throw;
+    }
+    if (str_len == 0) { 
+      delete[] str;
+      continue;
     }
     if (len_arr == size_arr) {
       char ** new_arr = nullptr;
       try {
         new_arr = new char*[size_arr + step_arr];
       } catch (const std::bad_alloc &) {
-        for (size_t i = 0; i < len_arr; ++i) {
-          delete[] arr_str[i];
-        }
-        delete[] arr_str;
+        zharov::destroyArr(arr_str, len_arr);
         throw;
       }
       for (size_t i = 0; i < len_arr; ++i) {
@@ -117,7 +150,7 @@ char * zharov::getLine(std::istream & in, size_t size, size_t step, size_t & len
 
   char * str = new char[size + 1];
   char sym = ' ';
-  while (in >> sym && sym != end) {
+  while (in >> sym && sym != end && sym != '\n') {
     str[len] = sym;
     ++len;
     if (size == len) {
@@ -129,6 +162,9 @@ char * zharov::getLine(std::istream & in, size_t size, size_t step, size_t & len
       }
       size += step;
     }
+  }
+  if (sym == '\n') {
+    in.putback(sym);
   }
   str[len] = '\0';
 
@@ -157,7 +193,7 @@ char * zharov::ShtSym(const char * str, char * res)
   size_t c = 0;
   for (char letter = 'a'; letter <= 'z'; letter++){
     for (size_t i = 0; str[i] != '\0'; ++i) {
-      if (letter == (str[i])) {
+      if (letter == tolower((str[i]))) {
         found = true;
         break;
       }
