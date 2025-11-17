c1a4e7b7906a566759f7bb463fa5003acd24643a
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-11-17 09:08:25 +0300

    create splitLine without try and extend arr_str

--- zharov.danil/P4/main.cpp
+++ zharov.danil/P4/main.cpp
@@ -57,29 +57,39 @@ void zharov::extend(char ** str, size_t old_size, size_t new_size)
   * str = new_str;
 }
 
-char ** splitLine(char * str, const char sep)
+char ** splitLine(char * str, const char sep, size_t size, size_t step, size_t & len)
 {
-  size_t c = 0;
+  char ** arr_str = new char * [size];
   bool is_last_space = true; 
+  size_t c = 0;
   size_t i = 0;
+  size_t start_i = 0;
   for (; str[i] != '\0'; ++i) {
-    if (str[i] == " ") {
+    if (str[i] == sep) {
       if (!is_last_space) {
-        ++c
+        char * new_str = new char[i - start_i + 1];
+        for (size_t j = 0; j < i - start_i; ++j) {
+          new_str[j] = str[start_i + j];
+        }
+        arr_str[c] = new_str;
+        ++c;
+        is_last_space = true;
       }
+      start_i = i + 1;
     } else {
       is_last_space = false;
     }
   }
-  if (str[i-1] != " ") {
+  if (str[i-1] != sep) {
+    char * new_str = new char[i - start_i + 1];
+    for (size_t j = 0; j < i - start_i; ++j) {
+      new_str[j] = str[start_i + j];
+    }
+    arr_str[c] = new_str;
     ++c;
   }
 
-  char ** arr_str = new char * [c];
-  i = 0;
-  for (; str[i] != '\0'; ++i) {
-    
-  }
+
 }
 
 char * zharov::getLine(std::istream & in, size_t size, size_t k, size_t & len, char end)
