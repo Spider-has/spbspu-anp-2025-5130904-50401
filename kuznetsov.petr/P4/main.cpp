556e567497d9bbb94e0fc64f0d7fbda4b2f6feb4
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-11-19 01:01:15 +0300

    added removeVow

--- kuznetsov.petr/P4/main.cpp
+++ kuznetsov.petr/P4/main.cpp
@@ -1,37 +1,62 @@
 #include <iostream>
 #include <cctype>
 #include <iomanip>
-#include <cstring>
+
 
 namespace kuznetsov {
   char* getLine(std::istream& in, size_t& size);
   void extend(char** str, size_t oldSize, size_t newSize);
+  void removeVow(char* buff, const char* str);
+  int seqSym();
+
 }
 
-// new/delete	RMV-VOW	SEQ-SYM
+// new/delete  RMV-VOW  SEQ-SYM
 int main()
 {
+  namespace kuz = kuznetsov;
+  size_t size = 0;
+  char* str = nullptr;
+  try {
+    str = kuz::getLine(std::cin, size);
+  } catch (const std::bad_alloc& e){
+    std::cerr << "Bad alloc " << e.what() << '\n';
+    return 1;
+  }
+  char* buffer = new char[size];
+  kuz::removeVow(buffer, str);
 
+  std::cout << buffer << '\n';
+
+  delete[] buffer;
+  delete[] str;
 }
 
 char* kuznetsov::getLine(std::istream& in, size_t& size)
 {
   char* buff = new char[8];
+  size = 8;
   bool is_skinws = in.flags() & std::ios::skipws;
   if (is_skinws) {
     in >> std::noskipws;
   }
   size_t i = 0;
-  while ( in >> buff[i] && i < size) {
+  while ( in >> buff[i] && buff[i] != '\n') {
+    if (i + 4 >= size) {
+      try {
+        extend(&buff, size, size+8);
+      } catch (const std::bad_alloc& e) {
+        delete[] buff;
+        throw;
+      }
+      size += 8;
+    }
     ++i;
   }
-
   buff[i] = '\0';
-
   if (is_skinws) {
     in >> std::skipws;
   }
-
   return buff;
 }
 
@@ -48,3 +73,20 @@ void kuznetsov::extend(char** str, size_t oldSize, size_t newSize)
   *str = extendedStr;
 }
 
+void kuznetsov::removeVow(char* buff, const char* str)
+{
+  char vow[] = "aeiou";
+  size_t nextPast = 0;
+  for (size_t i = 0; *(str + i) != '\0'; ++i ) {
+    size_t j = 0;
+    for (; j < 5; ++j) {
+      if (tolower(str[i]) == vow[j]) {
+        break;
+      }
+    }
+    if (j == 5) {
+      buff[nextPast] = str[i];
+      nextPast++;
+    }
+  }
+}
