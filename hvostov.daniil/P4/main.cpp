6fae1650b5d4c4e09053b26dca7de096309a932c
PodySIN <daniil2007xboctob@gmail.com>
2025-11-22 21:15:15 +0300

    Start creating input

--- hvostov.daniil/P4/main.cpp
+++ hvostov.daniil/P4/main.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <iomanip>
+
+size_t getLine(std::istream & in, char * data, size_t size) { bool is_skipws = in.flags() & std::ios_base::skipws; if (is_skipws);
+
 int main()
 {
 }
