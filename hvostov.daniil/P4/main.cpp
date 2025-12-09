40d1f6ab5ec73f58729f854ea97f74ecfbca3d53
PodySIN <daniil2007xboctob@gmail.com>
2025-12-10 01:20:52 +0300

    Fix errors, add qulificators, improve extend function, remove extension

--- hvostov.daniil/P4/main.cpp
+++ hvostov.daniil/P4/main.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 #include <iomanip>
 #include <cctype>
+#include <cmath>
 
 namespace hvostov {
-  char * getLine(std::istream & in, size_t & size, size_t extansion);
-  void extendStr(char ** str, size_t & size, size_t new_size);
-  void strConcatCharByChar(char * buffer, char * str1, char * str2);
-  void concatRemainders(char * str1, char * str2, size_t & i);
-  size_t countAlphaCharacters(char * str);
+  char * getLine(std::istream & in, size_t & size);
+  void resizeStr(char ** str, size_t & size, size_t new_size);
+  void extendStr(char ** str, size_t & size);
+  size_t getNewSize(size_t size);
+  void strConcatCharByChar(char * buffer, const char * str1, const char * str2);
+  void concatRemainders(char * str1, const char * str2, size_t & i);
+  size_t countAlphaCharacters(const char * str);
 }
 
 int main()
 {
   char * str = nullptr;
-  size_t size = 10, extansion = 5;
-  try {
-    str = hvostov::getLine(std::cin, size, extansion);
-    if (!std::cin) {
-      std::cerr << "Problems with input!\n";
-      delete[] str;
-      return 1;
-    }
-  } catch (const std::bad_alloc & e) {
-    std::cerr << e.what() << "\n";
+  size_t size = 10;
+  str = hvostov::getLine(std::cin, size);
+  if (str == nullptr) {
+    std::cerr << "Bad alloc!\n";
+    return 1;
+  } else if (!std::cin) {
+    std::cerr << "Problems with input!\n";
     delete[] str;
     return 1;
   }
@@ -30,7 +30,7 @@ int main()
   size_t size2 = 11;
   char * result = nullptr;
   try {
-    result = new char[size+size2+1];
+    result = new char[size + size2 + 1];
   } catch (const std::bad_alloc & e) {
     std::cerr << e.what() << "\n";
     return 1;
@@ -44,7 +44,7 @@ int main()
   return 0;
 }
 
-char * hvostov::getLine(std::istream & in, size_t & size, size_t extansion)
+char * hvostov::getLine(std::istream & in, size_t & size)
 {
   char * str = new char[size + 1];
   bool is_skipws = in.flags() & std::ios_base::skipws;
@@ -56,10 +56,10 @@ char * hvostov::getLine(std::istream & in, size_t & size, size_t extansion)
   while (in >> a && a != '\n') {
     if (i == size) {
       try {
-        hvostov::extendStr(& str, size, size + extansion + 1);
+        hvostov::extendStr(&str, size);
       } catch (const std::bad_alloc &) {
         delete[] str;
-        throw;
+        return nullptr;
       }
     }
     str[i] = a;
@@ -67,24 +67,48 @@ char * hvostov::getLine(std::istream & in, size_t & size, size_t extansion)
   }
   str[i] = '\0';
   size = i;
+  try {
+    hvostov::resizeStr(&str, size, size + 1);
+  } catch (const std::bad_alloc &) {
+    delete[] str;
+    return nullptr;
+  }
   if (is_skipws) {
     in >> std::skipws;
   }
   return str;
 }
 
-void hvostov::extendStr(char ** str, size_t & size, size_t new_size)
+void hvostov::resizeStr(char ** str, size_t & size, size_t new_size)
 {
   char * new_str = new char[new_size];
-  for (size_t i = 0; i < size; i++) {
-    new_str[i] = (* str)[i];
+  for (size_t i = 0; i < std::min(size, new_size); i++) {
+    new_str[i] = (*str)[i];
   }
   delete[] * str;
   size = new_size;
-  * str = new_str;
+  *str = new_str;
 }
 
-void hvostov::strConcatCharByChar(char * buffer, char * str1, char * str2)
+size_t hvostov::getNewSize(size_t size)
+{
+  double new_size = size * std::sqrt(2);
+  return std::ceil(new_size);
+}
+
+void hvostov::extendStr(char ** str, size_t & size)
+{
+  size_t new_size = hvostov::getNewSize(size);
+  char * new_str = new char[new_size];
+  for (size_t i = 0; i < size; i++) {
+    new_str[i] = (*str)[i];
+  }
+  delete[] * str;
+  size = new_size;
+  *str = new_str;
+}
+
+void hvostov::strConcatCharByChar(char * buffer, const char * str1, const char * str2)
 {
   size_t i = 0;
   for (; str1[i / 2] != '\0' && str2[i / 2] != '\0'; i++) {
@@ -94,12 +118,12 @@ void hvostov::strConcatCharByChar(char * buffer, char * str1, char * str2)
       buffer[i] = str2[i / 2];
     }
   }
-  char * remainder_str = str1[i / 2] == '\0' ? str2 : str1;
+  const char * remainder_str = str1[i / 2] == '\0' ? str2 : str1;
   hvostov::concatRemainders(buffer, remainder_str, i);
   buffer[i] = '\0';
 }
 
-void hvostov::concatRemainders(char * str1, char * str2, size_t & i)
+void hvostov::concatRemainders(char * str1, const char * str2, size_t & i)
 {
   size_t diff = i - i / 2;
   for (; str2[i - diff] != '\0'; i++) {
@@ -107,7 +131,7 @@ void hvostov::concatRemainders(char * str1, char * str2, size_t & i)
   }
 }
 
-size_t hvostov::countAlphaCharacters(char * str)
+size_t hvostov::countAlphaCharacters(const char * str)
 {
   size_t counter = 0;
   for (size_t i = 0; str[i] != '\0'; i++) {
